677debffd98cb3e24c3e2bd7683efce356e858a5
KKishore0106 <149188111+KKishore0106@users.noreply.github.com>
2025-03-22 00:14:52 +0530

    Update multiplediseaseprediction.py

--- multiplediseaseprediction.py
+++ multiplediseaseprediction.py
@@ -67,7 +67,7 @@ def get_prediction(disease, input_values):
         risk_level = "High" if prediction >= 0.7 else "Medium" if prediction >= 0.4 else "Low"
     else:
         return "⚠️ Model not available.", None
-     return f"Risk Level: {risk_level}", risk_level
+        return f"Risk Level: {risk_level}", risk_level
 except ValueError:
      return "⚠️ Invalid input detected. Please enter numeric values only.", None
 except Exception as e:
